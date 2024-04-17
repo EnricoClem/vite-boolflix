c37235f08488cf4b605027eaed2b0ef2e190a4c3
EnricoClem <enrico.clem94@gmail.com>
2024-04-17 16:13:03 +0200

    ADD - header search query and API connection

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,29 @@
 <template>
     <div class="container">
-        <input type="text" placeholder="Cerca film e serie tv...">
-        <button>Cerca!</button>
+        <input type="text" v-model="query" placeholder="Cerca film e serie tv...">
+        <button @click="searchData">Cerca!</button>
     </div>
 </template>
 
 <script>
+import { Axios } from 'axios';
+
 export default {
-    
+    data() {
+        return {
+            query:''
+        }
+    },
+    methods: {
+        searchData() {
+            axios.get('https://api.themoviedb.org/3/search/movie',{
+                params: {
+                    api_key: '8be08f5c8e1957ec16193b9e110877d2',
+                    query: this.query
+                }
+            })
+        }
+    }
 }
 </script>
 
