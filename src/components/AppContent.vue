a9076e85cea5f04269b290bec29e4c51ea3690ca
EnricoClem <enrico.clem94@gmail.com>
2024-04-17 15:57:33 +0200

    SETTING - base components structure

---
+++ src/components/AppContent.vue
@@ -0,0 +1,33 @@
+<template>
+    <main class="container">
+        <section>
+            <div class="container">
+                <h2>Risultati ricerca:</h2>
+            </div>
+            <div class="container">
+                <ul>
+                    <li>
+                        titolo del film
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+import { store } from '../store.js'
+
+export default {
+    data() {
+        return {
+            store: store,
+        }
+    },
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <input type="text" placeholder="Cerca film e serie tv...">
+        <button>Cerca!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
