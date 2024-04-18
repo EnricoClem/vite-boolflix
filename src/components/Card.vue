ecc022d450334515be561213e1b2bd6f325557d9
EnricoClem <enrico.clem94@gmail.com>
2024-04-18 15:28:44 +0200

    ADD - poster img

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,15 @@
 <template>
     <li class="card">
+        <img :src="`http://image.tmdb.org/t/p/w342/${item.poster_path}`" />
         <h3 v-if="item.title">{{ item.title }}</h3>
         <h3 v-else>{{ item.name }}</h3>
         <p v-if="item.original_title">{{ item.original_title }}</p>
         <p v-else>{{ item.original_name }}</p>
         <img v-if="languages[item.original_language]" :src="`/${languages[item.original_language]}`" class="lang-img" alt=""/>
-        <p v-else class="lang-initials">{{ item.original_language }}</p>
+        <div v-else>
+            <span>Lingua originale: </span>
+            <span class="lang-initials">{{ item.original_language }}</span>
+        </div>
         <p>{{ item.vote_average }}</p>
     </li>
 </template>
